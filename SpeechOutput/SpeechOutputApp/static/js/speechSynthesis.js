b977cca1895d4adafce4a4630643822f1914f95a
Kristin <kristincaras1@gmail.com>
2021-04-05 16:47:07 -0400

    added type to speech feature, project wireframe mock up

---
+++ SpeechOutput/SpeechOutputApp/static/js/speechSynthesis.js
@@ -0,0 +1,109 @@
+// //Utilizing the Web Speech API
+
+// Init SpeechSynth API
+const synth = window.speechSynthesis;
+
+// DOM Elements
+const textForm = document.querySelector("form");
+const textInput = document.querySelector("#text-input");
+const voiceSelect = document.querySelector("#voice-select");
+
+const body = document.querySelector("body");
+
+//Browser identifier
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== "undefined";
+
+// Chrome 1+
+var isChrome = !!window.chrome && !!window.chrome.webstore;
+
+// Init voices array
+let voices = [];
+
+const getVoices = () => {
+  voices = synth.getVoices();
+
+  // Loop through voices and create an option for each one
+  voices.forEach((voice) => {
+    // Create option element
+    const option = document.createElement("option");
+    // Fill option with voice and language
+    option.textContent = voice.name + "(" + voice.lang + ")";
+
+    // Set needed option attributes
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    voiceSelect.appendChild(option);
+  });
+};
+
+//Line 35, 36 causes voice list duplication
+
+getVoices();
+if (synth.onvoiceschanged !== undefined) {
+  synth.onvoiceschanged = getVoices;
+}
+
+//Fix for duplication, run code depending on the browser
+if (isFirefox) {
+  getVoices();
+}
+if (isChrome) {
+  if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = getVoices;
+  }
+}
+
+// Speak
+const speak = () => {
+  // Check if speaking
+  if (synth.speaking) {
+    console.error("Already speaking...");
+    return;
+  }
+  if (textInput.value !== "") {
+    // Get speak text
+    const speakText = new SpeechSynthesisUtterance(textInput.value);
+
+    // Speak end
+    speakText.onend = (e) => {
+      console.log("Done speaking...");
+    };
+
+    // Speak error
+    speakText.onerror = (e) => {
+      console.error("Something went wrong");
+    };
+
+    // Selected voice
+    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute(
+      "data-name"
+    );
+
+    // Loop through voices
+    voices.forEach((voice) => {
+      if (voice.name === selectedVoice) {
+        speakText.voice = voice;
+      }
+    });
+
+    // Set pitch and rate
+
+    // Speak
+    synth.speak(speakText);
+  }
+};
+
+// EVENT LISTENERS:
+
+// Text form submit for type to speak
+textForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  speak();
+  textInput.clear();
+});
+
+// Voice select change for type to speak
+voiceSelect.addEventListener("change", (e) => speak());
+
+//On click for picture book
