13d35df1cfcb5735f454f2cd9d9099c175205176
Kristin <kristincaras1@gmail.com>
2021-04-14 19:23:57 -0400

    working on speechToText.js file:

--- SpeechOutput/SpeechOutputApp/static/js/speechToText.js
+++ SpeechOutput/SpeechOutputApp/static/js/speechToText.js
@@ -1,6 +1,37 @@
 var speechRecognition = window.webkitSpeechRecognition;
 var recognition = new speechRecognition();
+recognition.continuous = true;
 
-var instructions = $("instructions");
+var instructions = $("#instructions");
 var generated_text = $("#generated_text");
 var content = "";
+
+recognition.onstart = function () {
+  instructions.text("Voice Recognition is on!");
+};
+
+recognition.onspeechend = function () {
+  instructions.text("Voice Recognition is off.");
+};
+
+recognition.onerror = function () {
+  instructions.text("Cannot detect voice, please try again.");
+};
+
+recognition.onresult = function (event) {
+  var current = event.resultIndex;
+  var transcript = event.results[current][0].transcript; //spoken data
+  content += transcript;
+  generated_text.val(content);
+};
+
+$("#start_button").click(function (event) {
+  if (content.length) {
+    content += "";
+  }
+  recognition.start();
+});
+
+generated_text.on("input", function () {
+  content = $(this).val();
+});
